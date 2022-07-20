7fd097ad96aec2ccbe0e6044f2ef689c03607ffb
Haruhito Fuji <haruhito.fuji@gmail.com>
2022-07-20 11:30:09 +0900

    アカウント情報を隠蔽するために修正

---
+++ skyway-callkit-sampler/Account.swift
@@ -0,0 +1,14 @@
+//
+//  Account.swift
+//  skyway-callkit-sampler
+//
+//  Created by 藤治仁 on 2022/07/20.
+//  Copyright © 2022 yorifuji. All rights reserved.
+//
+
+import UIKit
+
+class Account: NSObject {
+    static let skywayAPIKey:String?
+    static let skywayDomain:String?
+}
